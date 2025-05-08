d246cc6527df58c8846675507efc8869064b28ca
reggya <reggyamayang456@gmail.com>
2025-05-08 19:19:02 +0700

    [Feat] sent data products to detail cart

--- app/src/main/java/com/arbaelbarca/posfantastic/ui/presentation/navigation/HostNavigation.kt
+++ app/src/main/java/com/arbaelbarca/posfantastic/ui/presentation/navigation/HostNavigation.kt
@@ -1,7 +1,7 @@
 package com.arbaelbarca.posfantastic.ui.presentation.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -10,14 +10,21 @@ import com.arbaelbarca.posfantastic.ui.presentation.ui.screen.home.HomeScreen
 import com.arbaelbarca.posfantastic.ui.presentation.ui.screen.product.DetailProductScreen
 import com.arbaelbarca.posfantastic.ui.presentation.ui.screen.qris.QrisPaymentScreen
 import com.arbaelbarca.posfantastic.ui.presentation.ui.screen.success.SuccesPaymentScreen
+import com.arbaelbarca.posfantastic.ui.viewmodel.ProductViewModel
 
 @Composable
 fun HostNavigation() {
     val navController = rememberNavController()
 
     NavHost(navController = navController, startDestination = ObjectRouteScreen.HomeRoute.route) {
-        composable(ObjectRouteScreen.HomeRoute.route) { HomeScreen(navController) }
-        composable(ObjectRouteScreen.DetailProductScreenRoute.route) { DetailProductScreen(navController) }
+        composable(ObjectRouteScreen.HomeRoute.route) {
+            val productViewModel: ProductViewModel = hiltViewModel()
+            HomeScreen(navController, productViewModel)
+        }
+        composable(ObjectRouteScreen.DetailProductScreenRoute.route) {
+            val productViewModel: ProductViewModel = hiltViewModel()
+            DetailProductScreen(navController, productViewModel)
+        }
         composable(ObjectRouteScreen.QrisPaymentScreenRoute.route) { QrisPaymentScreen(navController) }
         composable(ObjectRouteScreen.SuccessPaymentScreenRoute.route) { SuccesPaymentScreen(navController, onShareInvoice = {}, onPrintInvoice = {}) }
 //        composable(ObjectRouteScreen.AddProductScreenRoute.route) { AddProductScreen(navController, onBackClick = {}, onPredictClick = {}, onSaveClick = {}) }
